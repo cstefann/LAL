8a8db7a916829adb7b45598b76028d64bf295e33
cstefan <33597024+StefanChimu@users.noreply.github.com>
2021-05-20 11:29:52 +0300

    Added more rules. (WIP)

--- main.py
+++ main.py
@@ -25,12 +25,21 @@ for i in tagged:
 template_string = """
 (deftemplate input
     (slot sentence (type STRING))
+    (slot answer (type STRING))
     (slot G1 (type STRING))
     (slot G2 (type STRING)) 
     (slot G3 (type STRING)) 
     (slot G4 (type STRING)) 
     (slot G5 (type STRING)) 
     (slot G6 (type STRING))
+)
+"""
+read_input_rule_string = """
+(defrule read_input
+    ?a <- (sentence)
+    =>
+    (assert (text S (explode$ (sentence))))
+    (retract ?a)
 )
 """
 
@@ -47,17 +56,38 @@ apply_rule_string = """
 )
 """
 
+succes_rule_string = """
+(defrule success
+    ?a <- (text EPS)
+    (answer $?steps)
+    =>
+    (printout t "YES" $?steps crlf)
+    (retract ?a)
+)
+"""
+
+fail_rule_string = """
+(defrule failure
+    ?a <- (text $?)
+    =>
+    (printout t "NO" crlf)
+    (retract ?a)
+)
+"""
+
 ''' -------------- '''
 
 env = clips.Environment()
  
 env.build(template_string)
+env.build(read_input_rule_string)
  
 template = env.find_template('input')
  
 fact = template.new_fact()
 fact.update({
     "sentence" : sentence_post_tagging,
+    "answer" : "",
     "G1" : "S NN A",
     "G2" : "A NN B",
     "G3" : "B NNP C",
@@ -69,3 +99,6 @@ fact.assertit()
  
 for fact in env.facts():
     print(fact)
+
+for rule in env.rules():
+    print(rule)
